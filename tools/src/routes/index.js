8a9bf40fdd79f911e1efa7caf65b386b66f039eb
FabioDevCode <0677384849@orange.fr>
2024-11-17 17:39:03 +0100

    Try to fix

--- tools/src/routes/index.js
+++ tools/src/routes/index.js
@@ -1,7 +1,7 @@
-
 import fs from "node:fs";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { fileURLToPath, pathToFileURL } from "node:url";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -13,15 +13,16 @@ export default async (app) => {
 	for (const file of fs.readdirSync(__dirname)) {
 		if (file !== "index.js") {
 			const routeConfig = routeConfigurations[file];
-			const routePath = routeConfig
-				? routeConfig.path
-				: `/${file.split(".")[0]}`;
+			const routePath = routeConfig ? routeConfig.path : `/${file.split(".")[0]}`;
 			const middlewares = routeConfig ? routeConfig.middlewares : [];
 			const filePath = path.join(__dirname, file);
 
 			try {
+				// Convertir le chemin en URL compatible avec l'importation ESM
+				const moduleUrl = pathToFileURL(filePath).href;
+
 				// Import du routeur du fichier
-				const module = await import(filePath);
+				const module = await import(moduleUrl);
 				const routes = module.default;
 
 				if (routes) {
